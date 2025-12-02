96424084285accc51f287a4c8f5b9750235e9c70
Evan <evan@brucemcrooster.dev>
2025-12-02 09:01:45 -0500

    Day 2 Part 1

---
+++ Sources/Day02.swift
@@ -0,0 +1,30 @@
+import Algorithms
+
+struct Day02: AdventDay {
+  var data: String
+
+  func part1() -> Any {
+    let matches = data.matches(of: /(?<start>\d+)-(?<end>\d+),?/)
+    
+    var sum = 0
+    
+    for match in matches {
+      for id in Int(String(match.output.start))!...Int(String(match.output.end))! {
+        let asString = String(id)
+        let length = asString.count
+        guard length & 1 == 0 else { continue }
+        
+        let firstHalf = asString.prefix(length / 2)
+        let secondHalf = asString.suffix(length / 2)
+        
+        if firstHalf == secondHalf { sum += id }
+      }
+    }
+    
+    return sum
+  }
+
+  func part2() -> Any {
+    return 0
+  }
+}
