8f6c31febaca6e287e2462e04542bb3766a138df
Evan <evan@brucemcrooster.dev>
2025-12-05 10:07:53 -0500

    Day 5 Part 2

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -27,6 +27,86 @@ struct Day05: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    let sections = data.split(separator: "\n\n", maxSplits: 1)
+    
+    let ranges: [ClosedRange<Int>] = sections[0]
+      .matches(of: /(?<lower>\d+)-(?<upper>\d+)/)
+      .map {
+        Int($0.output.lower)!...Int($0.output.upper)!
+      }
+    
+    let rangeData: [Int] = ranges
+      .reduce(into: []) { partialResult, range in
+        partialResult.append(range.lowerBound)
+        // Negative numbers will indicate the end of a range
+        partialResult.append(-range.upperBound)
+      }
+      .sorted { left, right in
+        // ordering positives (range starts) before negatives (range ends) solves an issue with
+        // single-integer ranges that start (and end) on the end of another range
+        if abs(left) != abs(right) { abs(left) < abs(right) } else { left > right }
+      }
+    
+    var currRangeDepth = 0
+    
+    var currRangeStart: Int?
+    
+    var bigRanges = [ClosedRange<Int>]()
+    
+    for rangeMarker in rangeData {
+      if rangeMarker >= 0 {
+        currRangeDepth += 1
+        if currRangeStart == nil {
+          assert(currRangeDepth == 1)
+          currRangeStart = rangeMarker
+        }
+      } else {
+        currRangeDepth -= 1
+        if currRangeDepth == 0 {
+          let start = currRangeStart!
+          let end = -rangeMarker
+          bigRanges.append(start...end)
+          currRangeStart = nil
+        }
+      }
+    }
+    
+    func sanityCheckBigRanges(smallRanges: [ClosedRange<Int>], bigRanges: [ClosedRange<Int>]) -> Bool {
+      var foundEnds = Array(repeating: (low: false, high: false), count: bigRanges.count)
+      
+      smallRangesLoop: for smallRange in smallRanges {
+        for (bigRangeIndex, bigRange) in bigRanges.enumerated() {
+          if bigRange.lowerBound <= smallRange.lowerBound && smallRange.upperBound <= bigRange.upperBound {
+            if bigRange.lowerBound == smallRange.lowerBound {
+              foundEnds[bigRangeIndex].low = true
+            }
+            if bigRange.upperBound == smallRange.upperBound {
+              foundEnds[bigRangeIndex].high = true
+            }
+            
+//            print("small range \(smallRange) was in big range \(bigRange)")
+            
+            continue smallRangesLoop
+          }
+        }
+        print("Small range \(smallRange) was not contained in any big range")
+        return false
+      }
+      
+      for (index, foundEnd) in foundEnds.enumerated() {
+        if !foundEnd.low || !foundEnd.high {
+          print("Failed to find ends for index \(index) (\(bigRanges[index]))")
+          return false
+        }
+      }
+      
+      return true
+    }
+    
+    assert(sanityCheckBigRanges(smallRanges: ranges, bigRanges: bigRanges))
+    
+    return bigRanges.reduce(0) { count, range in
+      count + range.count
+    }
   }
 }
--- Tests/Day05.swift
+++ Tests/Day05.swift
@@ -17,12 +17,30 @@ struct Day05Tests {
     17
     32
     """
+  
+  let overlappingSingleRangesTestInput = """
+    1-7
+    7-7
+    8-10
+    10-12
+    12-12
+    12-15
+    16-19
+    
+    5
+    9
+    11
+    12
+    20
+    """
 
   @Test func testPart1() async throws {
     #expect(String(describing: Day05(data: testInput1).part1()) == "3")
+    #expect(String(describing: Day05(data: overlappingSingleRangesTestInput).part1()) == "4")
   }
 
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day05(data: testInput1).part2()) == "14")
+    #expect(String(describing: Day05(data: overlappingSingleRangesTestInput).part2()) == "19")
   }
 }
