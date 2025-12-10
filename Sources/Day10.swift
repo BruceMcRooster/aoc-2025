e96657bc73103edeb9ee16c1c378939b2612a94d
Evan <evan@brucemcrooster.dev>
2025-12-10 14:06:58 -0500

    Day 10 Part 1

---
+++ Sources/Day10.swift
@@ -0,0 +1,92 @@
+import Algorithms
+import DequeModule
+import BitCollections
+
+fileprivate extension BitArray {
+  static func ^(lhs: BitArray, rhs: BitArray) -> BitArray {
+    precondition(lhs.count == rhs.count)
+    var result = BitArray()
+    for i in lhs.indices {
+      result.append(lhs[i] != rhs[i])
+    }
+    return result
+  }
+}
+
+struct Day10: AdventDay {
+  var data: String
+
+  struct Machine {
+    let target: BitArray
+    let buttons: Set<BitArray>
+    
+    init(fromLine line: some StringProtocol) {
+      var targetBitArray: BitArray = []
+      var allButtons: Set<BitArray> = []
+      
+      var currButtonBitArray: BitArray?
+              
+      
+      var currNum = 0
+      
+      for char in line {
+        switch char {
+        case "[", "]": continue
+        case ".": targetBitArray.append(false)
+        case "#": targetBitArray.append(true)
+        case "(": currButtonBitArray = BitArray(repeating: false, count: targetBitArray.count)
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9": currNum *= 10; currNum += Int(String(char))!
+        case ",": if currButtonBitArray != nil { // Allows us to ignore when done with buttons and onto joltage
+          currButtonBitArray![currNum] = true
+        }
+          currNum = 0
+        case ")": if currButtonBitArray != nil { // Allows us to ignore when done with buttons and onto joltage
+                    currButtonBitArray![currNum] = true
+                    currNum = 0
+                  }
+                  allButtons.insert(currButtonBitArray!); currButtonBitArray = nil
+        default: continue
+        }
+      }
+      
+      self.target = targetBitArray
+      self.buttons = allButtons
+    }
+  }
+  
+  func part1() -> Any {
+    let allMachines = data.split(separator: "\n").map(Machine.init(fromLine:))
+    
+    var sumOfPresses = 0
+    
+    for machine in allMachines {
+      // Stores the state, and the depth to get there
+      var statesToCheck: Deque<(from: BitArray, to: BitArray)> = []
+      
+      for button in machine.buttons {
+        statesToCheck.append((from: BitArray(repeating: false, count: machine.target.count), to: button))
+      }
+      
+      var foundStates: [BitArray: Int] = [.init(repeating: false, count: machine.target.count) : 0]
+      
+      while let next = statesToCheck.popFirst() {
+        let currDist = foundStates[next.from]! + 1
+        
+        if foundStates[next.to] == nil || foundStates[next.to]! > currDist {
+          foundStates[next.to] = currDist
+          for button in machine.buttons {
+            statesToCheck.append((from: next.to, to: next.to ^ button))
+          }
+        }
+      }
+      
+      sumOfPresses += foundStates[machine.target]!
+    }
+    
+    return sumOfPresses
+  }
+
+  func part2() -> Any {
+    return 0
+  }
+}
