019dfc44a777035d205ca8ad62c17d1fcd9eb6ee
Evan <evan@brucemcrooster.dev>
2025-12-10 12:52:34 -0500

    Day 9 Part 2 Failed miserably first time around (too slow and not correct). Got a little help reading the subreddit, and now this is even more correct than it needs to be for the shape of the challenge input

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -1,4 +1,5 @@
 import Algorithms
+import DequeModule
 
 struct Day09: AdventDay {
   var data: String
@@ -26,6 +27,192 @@ struct Day09: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    enum FloorTileType: Character {
+      case empty = "."
+      case red = "#"
+      case green = "X"
+    }
+    
+    func outlineBoard(redCorners: [(x: Int, y: Int)]) -> [[FloorTileType?]] {
+      // Index with board[x][y]
+      // nil will represent truly unknown
+      var board: [[FloorTileType?]] = [[nil]]
+      
+      var maxY: Int {
+        get {
+          assert(board.allSatisfy({ $0.count == board[0].count }), "maxY is not properly propogated")
+          return board[0].count - 1
+        }
+        set(newMaxY) {
+          for columnIndex in board.indices {
+            board[columnIndex].append(contentsOf: Array(repeating: nil, count: newMaxY - board[columnIndex].count + 1))
+          }
+          assert(maxY == newMaxY)
+        }
+      }
+      var maxX: Int {
+        get {
+          return board.count - 1
+        }
+        set(newMaxX) {
+          board += Array(repeating: Array(repeating: nil, count: maxY + 1), count: newMaxX - board.count + 1)
+          assert(maxX == newMaxX)
+        }
+      }
+      
+      func drawGreenLineBetween(_ startCorner: (x: Int, y: Int), and endCorner: (x: Int, y: Int)) {
+        if startCorner.x == endCorner.x {
+          for y in (min(startCorner.y, endCorner.y) + 1)..<max(startCorner.y, endCorner.y) {
+            board[startCorner.x][y] = .green
+          }
+        } else {
+          assert(startCorner.y == endCorner.y)
+          for x in (min(startCorner.x, endCorner.x) + 1)..<max(startCorner.x, endCorner.x) {
+            board[x][startCorner.y] = .green
+          }
+        }
+      }
+      
+      for (index, (x, y)) in redCorners.enumerated() {
+        if x > maxX {
+          maxX = x
+        }
+        if y > maxY {
+          maxY = y
+        }
+        
+        board[x][y] = .red
+        
+        guard index > 0 else { continue }
+        
+        let prevRedCorner = redCorners[index - 1]
+        drawGreenLineBetween(prevRedCorner, and: (x, y))
+      }
+      drawGreenLineBetween(redCorners.first!, and: redCorners.last!)
+      
+      
+      return board
+    }
+    
+    func printBoard(_ board: [[FloorTileType?]]) {
+      for y in 0..<board[0].count {
+        for x in 0..<board.count {
+          print(board[x][y]?.rawValue ?? ".", terminator: "")
+        }
+        print()
+      }
+    }
+    
+    var edges = [(start: (x: Int, y: Int), end: (x: Int, y: Int))]()
+    
+    var corner0: (x: Int, y: Int)?
+    
+    for (offset, line) in data.split(separator: "\n").enumerated() {
+      let x = Int(line.split(separator: ",")[0])!
+      let y = Int(line.split(separator: ",")[1])!
+      guard offset > 0 else { corner0 = (x, y); continue }
+      
+      if offset == 1 {
+        edges.append((start: corner0!, end: (x, y)))
+      } else {
+        edges.append((start: edges.last!.end, end: (x,y)))
+      }
+    }
+    edges.append((start: edges.last!.end, end: corner0!))
+    
+    let redCorners = edges.map({$0.start})
+    
+    let xValues = redCorners.map({$0.x}).sorted()
+    let yValues = redCorners.map({$0.y}).sorted()
+    
+    var compressedXLookupTable = [Int:Int]()
+    var currIndex = 0
+    for xValue in xValues where compressedXLookupTable[xValue] == nil {
+      compressedXLookupTable[xValue] = currIndex
+      currIndex += 2 // Do this to give some breathing room between walls
+    }
+    
+    var compressedYLookupTable = [Int:Int]()
+    currIndex = 0
+    for yValue in yValues where compressedYLookupTable[yValue] == nil {
+      compressedYLookupTable[yValue] = currIndex
+      currIndex += 2 // Do this to give some breathing room between walls
+    }
+    
+    let board = outlineBoard(redCorners: redCorners.map { (compressedXLookupTable[$0.x]!, compressedYLookupTable[$0.y]!) })
+    
+    func minAndMax(_ a: Int, _ b: Int) -> (min: Int, max: Int) {
+      return (min(a, b), max(a, b))
+    }
+    
+    // Inspired by https://www.reddit.com/r/adventofcode/comments/1pi3hff/2025_day_9_part_2_a_simple_method_spoiler
+    func intersectsAnyEdges(start: (x: Int, y: Int), end: (x: Int, y: Int)) -> Bool {
+      let (minX, maxX) = minAndMax(start.x, end.x)
+      let (minY, maxY) = minAndMax(start.y, end.y)
+      
+      for edge in edges {
+        let (edgeMinX, edgeMaxX) = minAndMax(edge.start.x, edge.end.x)
+        let (edgeMinY, edgeMaxY) = minAndMax(edge.start.y, edge.end.y)
+        if minX < edgeMaxX && maxX > edgeMinX && minY < edgeMaxY && maxY > edgeMinY {
+          return true
+        }
+      }
+      return false
+    }
+    
+    func areaOf(rectangle: [(x: Int, y: Int)]) -> Int {
+      let start = rectangle[0]
+      let end = rectangle[1]
+      return (abs(start.x - end.x) + 1) * (abs(start.y - end.y) + 1)
+    }
+    
+    func canFindExitPath(start: (x: Int, y: Int)) -> Bool {
+      let boardMaxX = board.count - 1
+      let boardMaxY = board[0].count - 1
+      
+      struct BoardPosition: Hashable, Equatable {
+        let x: Int
+        let y: Int
+      }
+      
+      var visited: Set<BoardPosition> = []
+      var queue: Deque<BoardPosition> = [.init(x: start.x, y: start.y)]
+      
+      while !queue.isEmpty {
+        let pos = queue.popLast()!
+        
+        guard visited.insert(pos).inserted else { /*Already in there*/ continue }
+        
+        if board[pos.x][pos.y] == .red || board[pos.x][pos.y] == .green {
+          continue
+        }
+        
+        if pos.x <= 0 || pos.y <= 0 || pos.x >= boardMaxX || pos.y >= boardMaxY  {
+          return true // Found a way to the edge
+        }
+        
+        queue.prepend(.init(x: pos.x - 1, y: pos.y))
+        queue.prepend(.init(x: pos.x + 1, y: pos.y))
+        queue.prepend(.init(x: pos.x, y: pos.y - 1))
+        queue.prepend(.init(x: pos.x, y: pos.y + 1))
+      }
+      
+      return false
+    }
+    
+    // Sort by largest area first, so we can return the first that works
+    rectangles: for rectangle in redCorners.combinations(ofCount: 2).sorted(by: { areaOf(rectangle: $0) > areaOf(rectangle: $1) }) {
+      guard !intersectsAnyEdges(start: rectangle[0], end: rectangle[1]) else { continue }
+      
+      let (minX, _) = minAndMax(compressedXLookupTable[rectangle[0].x]!, compressedXLookupTable[rectangle[1].x]!)
+      let (minY, _) = minAndMax(compressedYLookupTable[rectangle[0].y]!, compressedYLookupTable[rectangle[1].y]!)
+      
+      // DFS is not actually necessary for the real challenge due to the input shape, but it makes the algorithm always correct
+      guard !canFindExitPath(start: (minX + 1, minY + 1)) else { continue rectangles }
+      
+      return areaOf(rectangle: rectangle)
+    }
+    
+    fatalError("Should have found something")
   }
 }
--- Tests/Day09.swift
+++ Tests/Day09.swift
@@ -17,9 +17,62 @@ struct Day09Tests {
 
   @Test func testPart1() async throws {
     #expect(String(describing: Day09(data: testInput1).part1()) == "50")
+    #expect(String(describing: Day09(data: somewhatProblematicInput).part1()) == "121")
+    #expect(String(describing: Day09(data: potentiallyReallyProblematicInput).part1()) == "100")
   }
 
+  /*
+   ┌────────────┐
+   └───────────┐│
+               ││
+               ││
+               ││
+               ││
+               ││
+               └┘
+   Credit to https://www.reddit.com/r/adventofcode/comments/1pi3hff/comment/nt3qgh7/
+   */
+  let somewhatProblematicInput = """
+    1,1
+    11,1
+    11,11
+    10,11
+    10,2
+    1,2
+    """
+  /*
+    ┌────────┐
+    │┌──────┐│
+    ││      ││
+    ││      ││
+    ││      ││
+    ││      ││
+    └┘      ││
+       ┌────┘│
+    ┌──┘     │
+    └────────┘
+   */
+  // I'm worried that something will detect the inner rectangle formed by (2,2)x(9,7) or (2,7)x(9,2) (area 48)
+  // instead of the actual largest internal rectangle at the base with area 21
+  // simply raycasting doesn't work, since all points will hit a red or green tile before making it out
+  let potentiallyReallyProblematicInput = """
+    1,1
+    10,1
+    10,10
+    1,10
+    1,9
+    4,9
+    4,8
+    9,8
+    9,2
+    2,2
+    2,7
+    1,7
+    """
+  
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day09(data: testInput1).part2()) == "24")
+    #expect(String(describing: Day09(data: somewhatProblematicInput).part2()) == "22")
+    #expect(String(describing: Day09(data: potentiallyReallyProblematicInput).part2()) == "21")
   }
 }
