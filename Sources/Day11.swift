0c5c942fd3f20b9e1962bcdba546e8f8971a043f
Evan <evan@brucemcrooster.dev>
2025-12-15 14:15:48 -0500

    Day 11 Part 2

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -12,22 +12,77 @@ struct Day11: AdventDay {
       graph[String(colonSplit.first!)] = outputs
     }
     
-    var reachedCount = 0
-    
-    var currPlaces = ["you"]
-    
-    while !currPlaces.isEmpty {
-      let nextToContinue = currPlaces.popLast()!
-      
-      guard nextToContinue != "out" else { reachedCount += 1; continue }
-      
-      currPlaces.append(contentsOf: graph[nextToContinue]!)
-    }
-    
-    return reachedCount
+    return search(from: "you", for: "out", in: graph)
   }
 
+  // Thanks for inspiration from https://www.reddit.com/r/adventofcode/comments/1pjp1rm/comment/ntm4x2k/ to go backwards instead of forwards
+  // There are way too many paths to try a forward search
+  private func search(
+    from start: String,
+    for target: String,
+    in graph: [String: [String]]
+  ) -> Int {
+    var waysToGetThereCache = [start : 1]
+    
+    func numberOfWaysToGet(to innerTarget: String) -> Int {
+      if let knownWayToGetThere = waysToGetThereCache[innerTarget] {
+        return knownWayToGetThere
+      }
+      
+      var result = 0
+      
+      for prev in graph.keys where graph[prev]!.contains(innerTarget) {
+//        print("at \(innerTarget), checking previous \(prev)")
+        result += numberOfWaysToGet(to: prev)
+      }
+      
+      waysToGetThereCache[innerTarget] = result
+      
+      return result
+    }
+    
+    return numberOfWaysToGet(to: target)
+  }
+  
+  func generateMermaidChart(of graph: [String : [String]]) {
+    print("flowchart TD")
+    for (key, value) in graph {
+      for val in value {
+        print("\t\(key) --&gt; \(val)")
+      }
+    }
+  }
+  
   func part2() -> Any {
-    return 0
+    var graph = [String: [String]]()
+    
+    for line in data.split(separator: "\n") {
+      let colonSplit = line.split(separator: ": ")
+      let outputs = colonSplit[1].split(separator: " ").map(String.init)
+      graph[String(colonSplit.first!)] = outputs
+    }
+//    
+//    generateMermaidChart(of: graph)
+//    return 0
+        
+    let waysFromDACToFFT = search(from: "dac", for: "fft", in: graph)
+//    print("Found \(waysFromDACToFFT) ways to get from dac to fft")
+    let waysFromFFTToDAC = search(from: "fft", for: "dac", in: graph)
+//    print("Found \(waysFromFFTToDAC) ways to get from fft to dac")
+    
+    
+    if waysFromFFTToDAC == 0 {
+      let waysFromSVRToDAC = search(from: "svr", for: "dac", in: graph)
+      let waysFromFFTToOUT = search(from: "fft", for: "out", in: graph)
+      
+      return waysFromSVRToDAC * waysFromDACToFFT * waysFromFFTToOUT
+    } else {
+      assert(waysFromDACToFFT == 0)
+      
+      let waysFromSVRToFFT = search(from: "svr", for: "fft", in: graph)
+      let waysFromDACToOUT = search(from: "dac", for: "out", in: graph)
+      
+      return waysFromSVRToFFT * waysFromFFTToDAC * waysFromDACToOUT
+    }
   }
 }
--- Tests/Day11.swift
+++ Tests/Day11.swift
@@ -17,11 +17,27 @@ struct Day11Tests {
     iii: out
     """
 
+  let testInput2 = """
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out
+    """
+  
   @Test func testPart1() async throws {
     #expect(String(describing: Day11(data: testInput1).part1()) == "5")
   }
 
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day11(data: testInput2).part2()) == "2")
   }
 }
